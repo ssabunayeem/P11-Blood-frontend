cf9e8bad46cef3ce84b56a8f29fc6223cafc2866
ssabunayeem <ssabunayeem@gmail.com>
2025-12-17 21:49:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-gray-500'>
+        <h1>blood project</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/routes/Routes.jsx
@@ -0,0 +1,54 @@
+import { createBrowserRouter } from "react-router";
+
+
+
+import RootLayout from "../rootLayout/RootLayout";
+import Home from "../pages/Home";
+import Login from "./../pages/Login";
+import Register from "./../pages/Register";
+import DashboardLayout from "../DashbordLayout/DashboardLayout";
+import MainDashboardLayout from "../pages/MainDashboard/MainDashboardLayout";
+
+import ManageProduct from "../DashbordLayout/ManageProduct/ManageProduct";
+import AddRequest from "../DashbordLayout/AddRequest/AddRequest";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout></RootLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/signup",
+        element: <Register></Register>,
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <MainDashboardLayout />,
+      },
+      {
+        path: "add-request",
+        element: <AddRequest />,
+      },
+      {
+        path: "manage-products",
+        element: <ManageProduct />,
+      },
+    ],
+  },
+]);
+
+export default router;
